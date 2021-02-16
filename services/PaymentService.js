c56862bf68d1a08990c2751916bf4ed6a6e8e356
Richard Cham <ch.richard22@gmail.com>
2021-02-16 01:35:18 -0500

    p3

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -1,4 +1,8 @@
 const axios = require("axios");
+const mercadopago = require("mercadopago");
+mercadopago.configure({
+    access_token: 'APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439'
+});
 
 class PaymentService {
     constructor() {
